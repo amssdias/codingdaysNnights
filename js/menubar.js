ef2889488a94d0b86b4a971e6b3263bca098f93d
André Dias <andremssdias@hotmail.com>
2023-11-23 00:02:18 +0100

    Fix animation and scroll behavior from menu.

--- js/menubar.js
+++ js/menubar.js
@@ -1,10 +1,18 @@
-document.getElementById("navigation-bar__icon").addEventListener("click", function() {
-    let menuOptions = document.getElementById("navigation-bar__list");
+const menuOptions = document.getElementById("navigation-bar__list");
 
-    if (menuOptions.style.display === "none") {
+document.getElementById("navigation-bar__icon").addEventListener("click", toggleMenuOptions)
+
+// When clicking an item from the navigation bar
+const menuLinks = document.querySelectorAll(".navigation-bar__list__item__link");
+Array.from(menuLinks).forEach(function(menuOption) {
+    menuOption.addEventListener("click", toggleMenuOptions)
+})
+
+
+function toggleMenuOptions() {
+    if (menuOptions.style.display === "none" || !menuOptions.style.display) {
         menuOptions.style.display = "flex";
     } else {
         menuOptions.style.display = "none";
     }
-
-})
+}
